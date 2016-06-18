1316f5dbb67d8f94f639fa2c6d975b82c13e1dae
Lionel T <elrumordelaluz@hotmail.com>
2016-06-19 00:52:19 +0200

    Initial dist subtree commit

---
+++ main.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+var obj = {
+  a: 1,
+  b: 2
+};
+
+console.log(_extends({}, obj));
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1haW4uanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7OztBQUFBLElBQU0sTUFBTTtBQUNWLEtBQUcsQ0FETztBQUVWLEtBQUc7QUFGTyxDQUFaOztBQUtBLFFBQVEsR0FBUixjQUFpQixHQUFqQiIsImZpbGUiOiJtYWluLmpzIiwic291cmNlc0NvbnRlbnQiOlsiY29uc3Qgb2JqID0ge1xuICBhOiAxLFxuICBiOiAyXG59XG5cbmNvbnNvbGUubG9nKHsgLi4ub2JqIH0pXG4iXSwic291cmNlUm9vdCI6Ii9zb3VyY2UvIn0=
